bc4a44ffa0ec78dad37c14b383082963ae292f27
Carlos Tadeu <carlinhos.slz@hotmail.com>
2023-11-13 14:15:06 -0300

    first commit

--- templates/_cabecalho.php
+++ templates/_cabecalho.php
@@ -37,7 +37,10 @@
                         <li class="nav-item">
                             <a class="nav-link" href="/olhonailha/views/login.php">login</a>
                         </li>
-                       < <li class="nav-item">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/olhonailha/views/denuncia.php">Denuncia</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link active" href="/olhonailha/views/listaDenu.php">Teste</a>
                         </li>
                     </ul>
