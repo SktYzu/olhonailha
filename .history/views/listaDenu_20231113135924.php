d0d6981d2c110af520a94643ed218e988c93eb6d
SktYzu <frogster.af@gmail.com>
2023-11-13 14:43:14 -0300

    att

---
+++ .history/views/listaDenu_20231113135924.php
@@ -0,0 +1,26 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/olhonailha/templates/_cabecalho.php';
+?>
+
+
+
+<section class="container_denu">
+    <div class="card">
+        <div class="img_denu">
+            <img src="#" alt="">
+        </div>
+        <div class="titu_denu">
+            <h1>123</h1>
+        </div>
+        <div class="desc_denu">
+            123
+        </div>
+        <div class="loc_denu">
+            123
+        </div>
+        <div class="data_hora">
+            123
+        </div>
+
+    </div>
+</section>
