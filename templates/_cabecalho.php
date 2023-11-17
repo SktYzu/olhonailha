511030d3c2a793d1d92229287da01f70c6d373af
maryjamorim <mary_rose752@hotmail.com>
2023-11-17 17:02:09 -0300

    proj_integrador

--- templates/_cabecalho.php
+++ templates/_cabecalho.php
@@ -16,28 +16,41 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <header class="d-flex flex-column justify-content-end">
+
+        <div class="logo">
+            <a href="" class="btn btn-primary">LOGO</a>
+        </div>
+        <div class="cad">
+            <a href="" class="btn btn-primary">CADASTRE-SE</a>
+        </div>
+
+        <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/olhonailha/index.php">Olho na Ilha</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="/olhonailha/index.php">Inicio</a>
+                    <ul class="navbar-nav w-100 justify-content-evenly align-items-center">
+                        <li class="nav-item btn btn-danger ">
+                            <a class="nav-link text-white" aria-current="page" href="/olhonailha/views/denuncia.php">FAÇA UMA DENÚNCIA</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/olhonailha/views/faqs.php">FAQs</a>
+                            <a class="nav-link text-white" href="/olhonailha/views/feed.php">PROBLEMAS SOLUCIONADOS</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/olhonailha/views/admin/perfil.php">Perfil</a>
+                            <a class="nav-link text-white" href="/olhonailha/views/feed.php">DENÚNCIAS</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="/olhonailha/views/sobre.php">SOBRE</a>
+                        </li>
+
                     </ul>
                 </div>
             </div>
         </nav>
+        
     </header>
-
+    
+    
     <main>
